94faafbbd4e3ae539cf98b957ce730a7c855c0f4
unknown <gauravgarad7@gmail.com>
2025-01-17 10:27:37 +0530

    Updated HTML, CSS, and JS; added Images/; removed banner.jpg

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: [ "Developer", "Business Analyst","QA Tester", "Technical Support"],
+        strings: [ "Developer", "Data Analyst"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Developer","Business Analyst","QA Tester", "Technical Support"],
+        strings: [ "Developer", "Data Analyst"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
